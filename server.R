b2312abe0964dfe38b8f824e6fa93d28b8f7201e
nosson <jpaulson@Josephs-MacBook-Pro.local>
2014-06-29 23:03:30 -0400

    replacing csv with cleaned dataset

--- server.R
+++ server.R
@@ -1,36 +1,25 @@
 library(shiny)
 library(ggplot2)
 library(ggvis)
-dat = read.csv("Stunting3.csv")
-rownames(dat) = dat[,1]
-dat = dat[,c(1:12,15,18,21,24,26,27)]
-colnames(dat) = c("Country","Female parliament % (12)","LaborForceF (11)","LaborForceM (11)",
-                  "Maternal.Mortality.Ratio (10)","Adolescent fertility rate","Pop. 2nd Ed Female (06-10)",
-                  "Pop. 2nd Ed Male (06-10)","LaborForceF Participation (11)","LaborForceM Participation (11)",
-                  "GII (10)","GII (12)","2013 Global Hunger Index","Under-five Mortality rate (%)","Prevalence of undernourishment"
-                  ,"Prevalence of underweight in children under five years","MeanStuntingF","MeanStuntingM")
 
+load("data.rdata")
 all_values <- function(x) {
  if(is.null(x)) return(NULL)
  paste0(names(x)[1:2], ": ", format(x)[1:2], collapse = "<br />")
 }
 
 shinyServer(function(input, output) {
+
   df <- reactive({
-    data.frame(x=dat[,input$xaxis],y=dat[,input$yaxis],
-      country=dat[,1],xlab=input$xaxis,ylab=input$yaxis)
+    data.frame(x=df_complete[,input$xaxis],y=df_complete[,input$yaxis],
+      country=rownames(df_complete))
     })
   size <- reactive({
-    dat[,input$choice]
+    df_complete[,input$choice]
     })
-  xlab <- reactive({
-    as.character(input$xaxis)
-    })
-
 
   base <- df %>% ggvis(x=~x,y=~y) %>%
-  layer_points(size := size, fill.update
-     := "black", fill.hover := "red") %>% 
+  layer_points(size:=size,fill.update= ~factor(regions_complete)) %>% 
   layer_smooths(
     span = input_slider(0.2, 1, value = 1, step = 0.05, label = "span")
     ) %>% 
@@ -39,17 +28,19 @@ shinyServer(function(input, output) {
   base %>% add_tooltip(all_values,"hover") %>%
   bind_shiny("ggvis", "ggvis_ui")
 
-  xaxis <- reactive(
-    as.numeric(dat[,input$xaxis])
+  box <- reactive(
+    as.numeric(df_complete[,input$boxplot])
     )
-  yaxis <- reactive(
-    as.numeric(dat[,input$yaxis])
-    )
-  size <- reactive(
-    scaleFunction(as.numeric(dat[,input$size]))
-    )
+  # yaxis <- reactive(
+  #   as.numeric(df_complete[,input$yaxis])
+  #   )
+  # size <- reactive(
+  #   # scaleFunction(as.numeric(df_complete[,input$size]))
+  #   )
   
-output$plot<-renderPlot({
-  qplot(x=xaxis(),y=yaxis(),size=size(),xlab=input$xaxis,ylab=input$yaxis) + scale_size_continuous() + geom_smooth()
+output$boxp<-renderPlot({
+  # qplot(x=xaxis(),y=yaxis(),size=size(),xlab=input$xaxis,ylab=input$yaxis) + scale_size_continuous() + geom_smooth()
+  boxplot(box()~factor(regions_complete))
   })
+
  })
--- ui.R
+++ ui.R
@@ -1,29 +1,24 @@
 library(shiny)
 library(ggplot2)
 library(ggvis)
-dat = read.csv("Stunting3.csv",header=TRUE,stringsAsFactors=FALSE)
-dat = dat[,c(1:12,15,18,21,24,26,27)]
-colnames(dat) = c("Country","Female parliament % (12)","LaborForceF (11)","LaborForceM (11)",
-                  "Maternal.Mortality.Ratio (10)","Adolescent fertility rate","Pop. 2nd Ed Female (06-10)",
-                  "Pop. 2nd Ed Male (06-10)","LaborForceF Participation (11)","LaborForceM Participation (11)",
-                  "GII (10)","GII (12)","2013 Global Hunger Index","Under-five Mortality rate (%)","Prevalence of undernourishment"
-                  ,"Prevalence of underweight in children under five years","MeanStuntingF","MeanStuntingM")
+
+load("~/Desktop/helpmeviz/data.rdata")
 
 shinyUI(navbarPage("HelpMeViz",
   tabPanel("Scatter plot",sidebarLayout(sidebarPanel(
-    selectInput("yaxis","y-axis:",colnames(dat)[2:ncol(dat)],colnames(dat)[14]),
-    selectInput("xaxis","x-axis:",colnames(dat)[2:ncol(dat)],colnames(dat)[12]),
-    selectInput("choice","Size:",colnames(dat)[2:ncol(dat)],colnames(dat)[4]),
+    selectInput("yaxis","y-axis:",colnames(df_complete),colnames(df_complete)[1]),
+    selectInput("xaxis","x-axis:",colnames(df_complete),colnames(df_complete)[2]),
+    selectInput("choice","Size:",colnames(df_complete),colnames(df_complete)[3]),
     uiOutput("ggvis_ui")
   ),
   mainPanel(
     ggvisOutput("ggvis")
   ))),
-  tabPanel("Scatter plots",sidebarLayout(sidebarPanel(
-    selectInput("size","size:",colnames(dat)[2:ncol(dat)],colnames(dat)[4])
+  tabPanel("Box plots",sidebarLayout(sidebarPanel(
+    selectInput("boxplot","Choice:",colnames(df_complete),colnames(df_complete)[1])
     ),
     mainPanel(
-      plotOutput('plot')
+      plotOutput('boxp')
     )
     )
   )
